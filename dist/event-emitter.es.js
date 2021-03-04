5136e325ee2dab45f9957bd284745ee4fc3f0ec7
vladimir-xaro <vladimir.xaro@gmail.com>
2021-03-04 17:43:23 +0300

    update rollup config and builds

--- dist/event-emitter.es.js
+++ dist/event-emitter.es.js
@@ -1,47 +1,13 @@
-function __values(o) {
-    var s = typeof Symbol === "function" && Symbol.iterator, m = s && o[s], i = 0;
-    if (m) return m.call(o);
-    if (o && typeof o.length === "number") return {
-        next: function () {
-            if (o && i >= o.length) o = void 0;
-            return { value: o && o[i++], done: !o };
-        }
-    };
-    throw new TypeError(s ? "Object is not iterable." : "Symbol.iterator is not defined.");
-}
-function __read(o, n) {
-    var m = typeof Symbol === "function" && o[Symbol.iterator];
-    if (!m) return o;
-    var i = m.call(o), r, ar = [], e;
-    try {
-        while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
-    }
-    catch (error) { e = { error: error }; }
-    finally {
-        try {
-            if (r && !r.done && (m = i["return"])) m.call(i);
-        }
-        finally { if (e) throw e.error; }
-    }
-    return ar;
-}
-function __spreadArray(to, from) {
-    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
-        to[j] = from[i];
-    return to;
-}
-
-var EventEmitter = /** @class */ (function () {
+class EventEmitter {
     /**
      * Create Emitter
      */
-    function EventEmitter(on) {
-        if (on === void 0) { on = {}; }
+    constructor(on = {}) {
         /**
          * Event list
          */
         this.events = {};
-        for (var key in on) {
+        for (let key in on) {
             if (on[key]) {
                 this.subscribe(key, on[key]);
             }
@@ -50,184 +16,122 @@ var EventEmitter = /** @class */ (function () {
     /**
      * Creates a key for the event and subscribes the passed callback to it.
      */
-    EventEmitter.prototype.subscribe = function (key, cb) {
-        var e_1, _a;
-        var _this = this;
+    subscribe(key, cb) {
         if (!this.has(key)) {
             this.events[key] = [];
         }
-        var removes = [];
+        let removes = [];
         if (Array.isArray(cb)) {
-            try {
-                for (var cb_1 = __values(cb), cb_1_1 = cb_1.next(); !cb_1_1.done; cb_1_1 = cb_1.next()) {
-                    var _cb = cb_1_1.value;
-                    removes.push.apply(removes, __spreadArray([], __read(this.subscribe(key, _cb))));
-                }
-            }
-            catch (e_1_1) { e_1 = { error: e_1_1 }; }
-            finally {
-                try {
-                    if (cb_1_1 && !cb_1_1.done && (_a = cb_1.return)) _a.call(cb_1);
-                }
-                finally { if (e_1) throw e_1.error; }
+            for (const _cb of cb) {
+                removes.push(...this.subscribe(key, _cb));
             }
         }
         else {
             this.events[key].push(cb);
-            removes.push(function () { return _this.removeListener(key, cb); });
+            removes.push(() => this.removeListener(key, cb));
         }
         return removes;
-    };
+    }
     /**
      * Unsubscribes all callback functions from the event and removes the event
      * key.
      */
-    EventEmitter.prototype.unsubscribe = function () {
-        var e_2, _a;
-        var keys = [];
-        for (var _i = 0; _i < arguments.length; _i++) {
-            keys[_i] = arguments[_i];
-        }
-        try {
-            for (var keys_1 = __values(keys), keys_1_1 = keys_1.next(); !keys_1_1.done; keys_1_1 = keys_1.next()) {
-                var key = keys_1_1.value;
-                if (this.events[key]) {
-                    delete this.events[key];
-                }
+    unsubscribe(...keys) {
+        for (const key of keys) {
+            if (this.events[key]) {
+                delete this.events[key];
             }
         }
-        catch (e_2_1) { e_2 = { error: e_2_1 }; }
-        finally {
-            try {
-                if (keys_1_1 && !keys_1_1.done && (_a = keys_1.return)) _a.call(keys_1);
-            }
-            finally { if (e_2) throw e_2.error; }
-        }
-    };
+    }
     /**
      * Removes a specific event key callback function.
      */
-    EventEmitter.prototype.removeListener = function (key, cb) {
+    removeListener(key, cb) {
         // if (typeof this.events[key] === 'object') {
         if (Array.isArray(this.events[key])) {
-            var idx = this.events[key].indexOf(cb);
+            const idx = this.events[key].indexOf(cb);
             if (idx > -1) {
                 this.events[key].splice(idx, 1);
             }
         }
-    };
+    }
     /**
      * Calls the callback function only once, and then removes it.
      */
-    EventEmitter.prototype.once = function (key, cb) {
-        var remove = this.subscribe(key, function () {
+    once(key, cb) {
+        const remove = this.subscribe(key, () => {
             remove[0]();
-            Array.isArray(cb) ? cb.forEach(function (_cb) { return _cb(); }) : cb();
+            Array.isArray(cb) ? cb.forEach(_cb => _cb()) : cb();
         });
-    };
+    }
     /**
      * Checks for an event by key.
      * (Doesn't check for callback functions)
      */
-    EventEmitter.prototype.has = function (key) {
+    has(key) {
         return !!this.events[key];
-    };
+    }
     /**
      * Returns the number of callback functions for the event key or "false" if
      * there is no key
      */
-    EventEmitter.prototype.listenerCount = function (key) {
+    listenerCount(key) {
         if (!this.events.hasOwnProperty(key)) {
             return false;
         }
         return this.events[key].length;
-    };
+    }
     /**
      * Calls all callback functions on events using the event key.
      */
-    EventEmitter.prototype.emit = function (key) {
-        var e_3, _a;
-        var args = [];
-        for (var _i = 1; _i < arguments.length; _i++) {
-            args[_i - 1] = arguments[_i];
-        }
-        var event = this.events[key];
+    emit(key, ...args) {
+        const event = this.events[key];
         if (event) {
-            try {
-                for (var event_1 = __values(event), event_1_1 = event_1.next(); !event_1_1.done; event_1_1 = event_1.next()) {
-                    var cb = event_1_1.value;
-                    cb.apply(void 0, __spreadArray([], __read(args)));
-                }
-            }
-            catch (e_3_1) { e_3 = { error: e_3_1 }; }
-            finally {
-                try {
-                    if (event_1_1 && !event_1_1.done && (_a = event_1.return)) _a.call(event_1);
-                }
-                finally { if (e_3) throw e_3.error; }
+            for (let cb of event) {
+                cb(...args);
             }
         }
-    };
+    }
     /**
      * Just like "emit" calls all callback functions. However, the callback must
      * return a boolean value, which determines whether or not the next callback
      * will execute.
      * As a result, it returns the result of the last executed callback function.
      */
-    EventEmitter.prototype.validateEmit = function (key) {
-        var e_4, _a;
-        var args = [];
-        for (var _i = 1; _i < arguments.length; _i++) {
-            args[_i - 1] = arguments[_i];
-        }
-        var event = this.events[key];
+    validateEmit(key, ...args) {
+        const event = this.events[key];
         if (!event) {
             return false;
         }
-        try {
-            for (var event_2 = __values(event), event_2_1 = event_2.next(); !event_2_1.done; event_2_1 = event_2.next()) {
-                var cb = event_2_1.value;
-                if (!cb.apply(void 0, __spreadArray([], __read(args)))) {
-                    return false;
-                }
+        for (const cb of event) {
+            if (!cb(...args)) {
+                return false;
             }
-        }
-        catch (e_4_1) { e_4 = { error: e_4_1 }; }
-        finally {
-            try {
-                if (event_2_1 && !event_2_1.done && (_a = event_2.return)) _a.call(event_2);
-            }
-            finally { if (e_4) throw e_4.error; }
         }
         return true;
-    };
+    }
     /**
      * Just like "emit" calls all callbacks, but unlike "emit" it passes the
      * result of the previous callback to the next one as an argument.
      * As aresult, it will return the result of the last callback.
      */
-    EventEmitter.prototype.seriesEmit = function (key) {
-        var args = [];
-        for (var _i = 1; _i < arguments.length; _i++) {
-            args[_i - 1] = arguments[_i];
-        }
-        var event = this.events[key];
+    seriesEmit(key, ...args) {
+        const event = this.events[key];
         if (!event) {
             return;
         }
-        var params;
-        for (var i = 0; i < event.length; i++) {
+        let params;
+        for (let i = 0; i < event.length; i++) {
             if (i === 0) {
-                params = event[i].apply(event, __spreadArray([], __read(args)));
+                params = event[i](...args);
             }
             else {
                 params = event[i](params);
             }
         }
         return params;
-    };
-    return EventEmitter;
-}());
+    }
+}
 
 export default EventEmitter;
 //# sourceMappingURL=event-emitter.es.js.map
--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,96 @@ import { terser } from 'rollup-plugin-terser';
 // import commonjs from '@rollup/plugin-commonjs';
 import cleanup from 'rollup-plugin-cleanup';
 
+// export default CLIArgs => {
+//   const mode = process.env.BUILD || 'development';
+//   const isDev = mode === 'development';
+
+//   const name = 'EventEmitter';
+//   const filename = 'event-emitter';
+
+//   const baseOutput = {
+//     sourcemap: true,
+//     name,
+//   };
+//   let output = [];
+
+//   if (isDev) {
+//     output.push(Object.assign({}, baseOutput, {
+//       format: 'iife',
+//       file: `dev/${filename}.js`,
+//       plugins: [
+//         resolve({
+//           browser: true,
+//         }),
+//         typescript({
+//           target: 'es5'
+//         }),
+//       ]
+//     }));
+//   } else {
+//     output.push(
+//       Object.assign({}, baseOutput, {
+//         format: 'iife',
+//         file: `dist/${filename}.js`,
+//         plugins: [
+//           resolve({
+//             browser: true,
+//           }),
+//           typescript({
+//             target: 'es5'
+//           }),
+//         ]
+//       }),
+//       Object.assign({}, baseOutput, {
+//         format: 'iife',
+//         file: `dist/${filename}.min.js`,
+//         plugins: [
+//           resolve({
+//             browser: true,
+//           }),
+//           typescript({
+//             target: 'es5'
+//           }),
+//           terser(),
+//         ]
+//       }),
+//       Object.assign({}, baseOutput, {
+//         format: 'es',
+//         file: `dist/${filename}.es.js`,
+//         plugins: [
+//           typescript({
+//             target: 'esnext'
+//           }),
+//         ]
+//       }),
+//       Object.assign({}, baseOutput, {
+//         format: 'es',
+//         file: `dist/${filename}.es.min.js`,
+//         plugins: [
+//           typescript({
+//             target: 'esnext'
+//           }),
+//           terser(),
+//         ]
+//       }),
+//     );
+//   };
+
+//   return {
+//     input: 'src/index' + (isDev ? '.dev' : '') + '.ts',
+//     output,
+//     plugins: [
+//       // commonjs(),
+
+//       !isDev && cleanup({
+//         comments: 'none'
+//       }),
+//     ],
+//   }
+// };
+
+
+
 export default CLIArgs => {
   const mode = process.env.BUILD || 'development';
   const isDev = mode === 'development';
@@ -12,33 +102,82 @@ export default CLIArgs => {
   const name = 'EventEmitter';
   const filename = 'event-emitter';
 
-  const baseOutput = {
-    sourcemap: true,
-    name,
-  };
-  let output = [];
+  let result = [];
 
   if (isDev) {
-    output.push(Object.assign({}, baseOutput, {
-      format: 'iife',
-      file: `dev/${filename}.js`
-    }));
+    result.push({
+      input: 'src/index.dev.ts',
+      output: {
+        sourcemap: true,
+        name,
+        format: 'iife',
+        file: `dev/${filename}.js`,
+        plugins: [
+          resolve({
+            browser: true,
+          }),
+          typescript({
+            target: 'es5'
+          }),
+        ]
+      },
+      plugins: [],
+    })
   } else {
-    output.push(
+    const base = {
+      input: 'src/index.ts',
+      output: [],
+      plugins: [
+        cleanup({
+          comments: 'none'
+        }),
+      ],
+    };
+    const baseOutput = {
+      sourcemap: true,
+      name,
+    }
+
+    result.push(
+      Object.assign({}, base, {
+        plugins: [
+          resolve({
+            browser: true,
+          }),
+          typescript({
+            target: 'es5'
+          }),
+        ]
+      }),
+      Object.assign({}, base, {
+        plugins: [
+          typescript({
+            target: 'esnext'
+          }),
+        ]
+      }),
+    );
+
+    // es5
+    result[0].output.push(
       Object.assign({}, baseOutput, {
         format: 'iife',
         file: `dist/${filename}.js`
       }),
       Object.assign({}, baseOutput, {
         format: 'iife',
-        file: `dist/${filename}.min.js`,
+        file: `dist/${filename}.js`,
         plugins: [
           terser(),
         ]
       }),
+    );
+
+    // es
+    result[0].output.push(
       Object.assign({}, baseOutput, {
         format: 'es',
-        file: `dist/${filename}.es.js`
+        file: `dist/${filename}.es.js`,
       }),
       Object.assign({}, baseOutput, {
         format: 'es',
@@ -50,21 +189,5 @@ export default CLIArgs => {
     );
   }
 
-  return {
-    input: 'src/index' + (isDev ? '.dev' : '') + '.ts',
-    output,
-    plugins: [
-      resolve({
-        browser: true,
-      }),
-      typescript({
-        target: 'es5'
-      }),
-      // commonjs(),
-
-      !isDev && cleanup({
-        comments: 'none'
-      }),
-    ],
-  }
-};
+  return result;
+}
